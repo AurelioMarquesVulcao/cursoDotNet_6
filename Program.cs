15fce05573fbf06ef5bd511ceb400b6bac1bc82d
AurelioMarquesVulcao <vulcao14@gmail.com>
2022-06-22 22:31:17 -0300

    feat: novas aulas

--- Program.cs
+++ Program.cs
@@ -1,6 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World 2");
+app.MapGet("/", () => "Hello World 4");
+app.MapGet("/user", () => new
+{
+    Name = "Aurelio Vulcao"
+});
+app.MapGet("/AddHeader", () => "repos");
+
+app.MapPost("/saveproduct", (Product product) =>
+{
+    return product.Code + " - " + product.Name + " Foi cadastrado com sucesso";
+});
+
+
 
 app.Run();
+
+public class Product
+{
+    public string Code { get; set; }
+    public string Name { get; set; }
+}
